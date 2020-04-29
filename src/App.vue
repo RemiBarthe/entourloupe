16cd3aba4590037b49aad312b9f7957297baad6f
RemiBarthe <barthe.rem@gmail.com>
2020-04-30 00:31:45 +0200

    Test particule

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
   <v-app>
-    <v-content style="background-color:#f4f4f4">
+    <v-content style="background-color:#2C003E">
+      <vue-particles
+        color="#FFA372"
+        :particleOpacity="0.7"
+        :particlesNumber="110"
+        shapeType="circle"
+        :particleSize="3"
+        linesColor="#FFA372"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.3"
+        :linesDistance="100"
+        :moveSpeed="1"
+        :hoverEffect="false"
+        hoverMode="grab"
+        :clickEffect="false"
+        clickMode="false"
+      >
+      </vue-particles>
       <room v-if="currentUser" />
       <startGame v-else />
     </v-content>
