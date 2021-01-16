592309a4a2ac7bfefe376f98bfd0d3b16db25cd4
RemiBarthe <barthe.rem@gmail.com>
2021-01-16 19:00:50 +0100

    Get idRoom from url and add button to share it

--- src/main.js
+++ src/main.js
@@ -2,15 +2,19 @@ import Vue from 'vue'
 import vuetify from './plugins/vuetify'
 import Vuex from 'vuex'
 import VueParticles from 'vue-particles'
+import VueClipboard from 'vue-clipboard2'
 
 import App from './App.vue'
 import { store } from './store/store'
 import './firebase'
 
-
 Vue.config.productionTip = false
+VueClipboard.config.autoSetContainer = true
+
 Vue.use(Vuex)
 Vue.use(VueParticles)
+Vue.use(VueClipboard)
+
 
 new Vue({
   vuetify,
